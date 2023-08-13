ab37848dc9febd39e6cebbb93aa2ba09248d3e22
Julie C <jucheng925@gmail.com>
2023-08-13 18:48:23 -0400

    functioning app

--- src/components/App.js
+++ src/components/App.js
@@ -10,17 +10,15 @@ function App() {
   function addOrder(order) {
     setOrders(orders.concat(order));
   }
-
-  const displayOrders = orders.map((order, idx) => {
-    <Order key={idx} {...order} />;
-  });
+  console.log(orders)
+  const displayOrders = orders.map((order, idx) => <Order key={idx} {...order} />);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Form />
+      <Form addOrder={addOrder} />
       <div className="ui raised container segment">
         <h1 className="ui block header">All Orders</h1>
         <div className="ui three cards">{displayOrders}</div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -11,12 +11,12 @@ const DEFAULT_STATE = {
   sides: [],
 };
 
-function Form() {
+function Form({addOrder}) {
   const [formState, setFormState] = useState(DEFAULT_STATE);
 
-  function handleSubmit() {
+  function handleSubmit(event) {
     event.preventDefault();
-    props.addOrder(formState);
+    addOrder(formState);
 
     setFormState({
       ...DEFAULT_STATE,
@@ -24,9 +24,10 @@ function Form() {
     event.target.reset();
   }
 
-  function handleChange() {
+  function handleChange(event) {
     const itemType = event.target.name;
     const item = event.target.value;
+
 
     if (formState[itemType].includes(item)) {
       setFormState({
